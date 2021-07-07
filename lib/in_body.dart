38165c5d97edd9aed8efa6c8d2bd7249670a3cbd
pranav4-4 <60073163+pranav4-4@users.noreply.github.com>
2021-07-07 14:30:28 +0530

    Add files via upload

---
+++ lib/in_body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:shop_with_me/title_of_project.dart';
+import 'color.dart';
+import 'const.dart';
+import 'discip.dart';
+import 'item_counter.dart';
+import 'my_cart.dart';
+import 'myprdt.dart';
+class Hello extends StatelessWidget {
+  final Product product;
+
+  const Hello({Key key, this.product}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // It provide us total height and width
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.3),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPaddin,
+                    right: kDefaultPaddin,
+                  ),
+                  // height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      ColorAndSize(product: product),
+                      SizedBox(height: kDefaultPaddin / 2),
+                      Description(product: product),
+                      SizedBox(height: kDefaultPaddin / 2),
+                      CounterWithFavBtn(),
+                      SizedBox(height: kDefaultPaddin / 2),
+                      AddToCart(product: product)
+                    ],
+                  ),
+                ),
+                ProductTitleWithImage(product: product)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'const.dart';
+import 'my_home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'hello',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeSc(),
+    );
+  }
+}
